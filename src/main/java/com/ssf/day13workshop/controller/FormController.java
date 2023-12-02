c53b1f56287fe34aa6355914170167e57e457d60
Megan <megan.koh96@gmail.com>
2023-12-02 22:12:51 +0800

    Initial Commit

--- src/main/java/com/ssf/day13workshop/controller/FormController.java
+++ src/main/java/com/ssf/day13workshop/controller/FormController.java
@@ -1,6 +1,9 @@
 package com.ssf.day13workshop.controller;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +11,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -44,9 +48,28 @@ public class FormController {
             String addressBookDirPath = Day13WorkshopApplication.getAddressBookDirPath();
             contacts.save(user, null, addressBookDirPath);
             System.out.println("Contact saved in: " + addressBookDirPath);
-        } catch (IOException e){
+        } catch (IOException e) {
             return "error";
         }
         return "contact-created";
+    }
+
+    @GetMapping("/contact/{id}")
+    public String loadID(@PathVariable String id, Model model) {
+        String addressBookDirPath = Day13WorkshopApplication.getAddressBookDirPath();
+        String filePath = addressBookDirPath + "/" + id + ".txt";
+        
+        if (Files.exists(Paths.get(filePath))){
+        try {
+            List<String> userFields = Files.readAllLines(Paths.get(filePath));
+            model.addAttribute("userFields", userFields);
+            return "contact-by-id";
+        } catch (IOException e){
+            return "error";
+        }
+        } else {
+            return "not-found";
+        }
+
     }
 }
